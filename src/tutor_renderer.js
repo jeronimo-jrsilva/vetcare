a1067cf13fec31acca633beaf5d247f9e6791a56
rafaelaroliveira <rafaaoli71@gmail.com>
2026-08-13 23:30:47 +0000

    event listener na janela dos tutores

--- src/tutor_renderer.js
+++ src/tutor_renderer.js
@@ -1,2 +1,23 @@
 // Lógica para cadastro de tutores e comunicação com o banco de dados
 console.log("Tutor Renderer Carregado!");
+
+const form = document.getElementById('form-tutor');
+const lista = document.getElementById('lista');
+
+form.addEventListener ('submit', function (e) {
+    e.preventDefault();
+});
+
+const nome = document.getElementById('nome').value;
+const telefone = document.getElementById('telefone').value;
+const email = document.getElementById('email').value;
+const cpf = document.getElementById('cpf').value;
+const endereco = document.getElementById('endereco').value;
+const petNome = document.getElementById('petNome').value;
+const petIdade = document.getElementById('petIdade').value;
+
+const item = document.createElement("li");
+  item.textContent = nome + " - " + telefone + " - " + email + " - " + cpf + " - Pet: " + petNome + " (" + petIdade + " anos)";
+
+  lista.appendChild(item);
+  form.reset();
