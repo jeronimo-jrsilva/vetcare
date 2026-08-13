a55ea1d8fe9db7dbcc41c636d19e7fa9d4f1bf30
Henrique <marcoshenriqueoliveira48@gmail.com>
2026-08-13 23:53:48 +0000

    Atualizção_parte2_cadastropets

--- src/pet_renderer.js
+++ src/pet_renderer.js
@@ -1,9 +1,9 @@
 // Lógica para cadastro de pets e comunicação com o banco de dados
 console.log("Pet Renderer Carregado!");
-const formcad_pet = document.getElementById('form-cad_pet');
-const inputnome = document.getElementById('nome');
-const inputraca = document.getElementById('raca');
-const inputgenero = document.getElementById('genero');
-const inputDatadenascimento = document.getElementById('Datadenascimento');
-const inputobservacao = document.getElementById('observacao');
+const formcad_pet = document.querySelector('form-cad_pet');
+const inputnome = document.querySelector('nome');
+const inputraca = document.querySelector('raca');
+const inputgenero = document.querySelector('genero');
+const inputDatadenascimento = document.querySelector('Datadenascimento');
+const inputobservacao = document.querySelector('observacao');
 
