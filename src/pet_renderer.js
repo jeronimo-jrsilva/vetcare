beb694ada2f64a274113aeea18ed184ee1928b1d
Henrique <marcoshenriqueoliveira48@gmail.com>
2026-08-13 23:42:48 +0000

    id_renderer_cadastropet

--- src/pet_renderer.js
+++ src/pet_renderer.js
@@ -1,2 +1,8 @@
 // Lógica para cadastro de pets e comunicação com o banco de dados
 console.log("Pet Renderer Carregado!");
+const formcad_pet = document.getElementById('form-cad_pet');
+const inputnome = document.getElementById('nome');
+const inputraca = document.getElementById('raca');
+const inputgenero = document.getElementById('genero');
+const inputDatadenascimento = document.getElementById('Datadenascimento');
+const inputobservacao = document.getElementById('observacao');
