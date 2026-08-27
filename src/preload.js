677913f9310bc38af85b9478d8dd37da55cd8770
Rafael Oliveira Marques <RafaelKiev47@gmail.com>
2026-08-27 23:47:29 +0000

    refatoração do preload.js: correção de nomes de funções para seguir convenções de nomenclatura e reorganização de código

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Exposição segura de IPC para os scripts de renderização (renderers)
 contextBridge.exposeInMainWorld('api', {
   // Login
   loginUsuario: (dados) => ipcRenderer.invoke('login-usuario', dados),
+  cadastrarUsuario: (dados) => ipcRenderer.invoke('cadastrar-usuario', dados),
 
   // Tutores
   listarTutores: () => ipcRenderer.invoke('listar-tutores'),
@@ -13,22 +13,19 @@ contextBridge.exposeInMainWorld('api', {
 
   // Pets
   listarPets: () => ipcRenderer.invoke('listar-pets'),
-  cadastrarpet: (dados) => ipcRenderer.invoke('cadastrar-pet', dados),
-  editarpet: (id, dados) => ipcRenderer.invoke('editar-pet', id, dados),
-  excluirpet: (id) => ipcRenderer.invoke('excluir-pet', id),
-  buscarpets: (termo) => ipcRenderer.invoke('buscar-pet', termo),
+  cadastrarPet: (dados) => ipcRenderer.invoke('cadastrar-pet', dados),
+  editarPet: (id, dados) => ipcRenderer.invoke('editar-pet', id, dados),
+  excluirPet: (id) => ipcRenderer.invoke('excluir-pet', id),
+  buscarPets: (termo) => ipcRenderer.invoke('buscar-pet', termo),
 
   // Agendamentos
   listarAgendamentos: () => ipcRenderer.invoke('listar-agendamentos'),
   criarAgendamento: (dados) => ipcRenderer.invoke('criar-agendamento', dados),
+  editarAgendamento: (id, dados) => ipcRenderer.invoke('editar-agendamento', id, dados),
   excluirAgendamento: (id) => ipcRenderer.invoke('excluir-agendamento', id),
 
   // Histórico Clínico
-listarHistoricoPet: (idPet) => ipcRenderer.invoke('listar-historico-pet', idPet),
-cadastrarHistorico: (dados) => ipcRenderer.invoke('cadastrar-historico', dados),
-excluirHistorico: (id) => ipcRenderer.invoke('excluir-historico', id),
-   // Usuário (Login)
-  loginUsuario: (dados) => ipcRenderer.invoke('login-usuario', dados),
-  cadastrarUsuario: (dados) => ipcRenderer.invoke('cadastrar-usuario', dados),
+  listarHistoricoPet: (idPet) => ipcRenderer.invoke('listar-historico-pet', idPet),
+  cadastrarHistorico: (dados) => ipcRenderer.invoke('cadastrar-historico', dados),
+  excluirHistorico: (id) => ipcRenderer.invoke('excluir-historico', id),
 });
-
